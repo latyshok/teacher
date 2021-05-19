7ed478d2df6b61e8f86b547be95402949105b4c0
Kirill <kirillkrut.vladimirov@yandex.ru>
2021-05-19 21:20:26 +0300

    task_2

---
+++ task_2/script.js
@@ -0,0 +1,21 @@
+user();
+
+function user() {
+	let vasya = {
+		name: "Вася",
+	}
+
+	alert(`Привет, меня зовут ${vasya.name}.`);
+
+	let petya = {
+		name: "Петя",
+	}
+
+	alert(`Привет, меня зовут ${petya.name}.`);
+
+	let kirill = {
+		name: "Кирилл",
+	}
+
+	alert(`Привет, меня зовут ${kirill.name}.`);
+}

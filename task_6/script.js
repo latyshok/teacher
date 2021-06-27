fcc063a8e98fcb7c8673f0ead4085a43d0e420c1
Kirill Vladimirov <kirillkrut.vladimirov@yandex.ru>
2021-06-27 12:57:34 +0300

    добавлен метод REDUCE

--- task_6/script.js
+++ task_6/script.js
@@ -9,22 +9,16 @@ let men = [
 let commonAge = getCommonAge(men);
 alert(commonAge);
 
-let sortByAge = men.sort((p1, p2) => p1.age - p2.age);
+men.sort((p1, p2) => p1.age - p2.age);
 
 let someUsers = men.filter(item => item.name != "Kolya");
-console.log(someUsers);
-
 alert(getCommonAge(someUsers));
 
 
 
 function getCommonAge(men) {
-	let newArr = men.map(value => value.age);
-	let sum = 0;
-
-	for (let number of newArr) {
-		sum += number;
-	}
-
-	return sum;
+	let newArr = men.reduce((sum, item) => sum + item.age, 0)
+	return newArr;
 }
+
+
